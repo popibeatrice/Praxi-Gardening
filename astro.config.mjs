858a8b8a2e382d653468ed79c9c47ac8d9847288
DavidPav3l <david_marian@outlook.com>
2023-10-29 23:16:06 +0200

    the starting start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,36 @@
 import { defineConfig } from 'astro/config';
 import NetlifyCMS from 'astro-netlify-cms';
 import tailwind from '@astrojs/tailwind';
+import compress from "astro-compress";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    NetlifyCMS({
-      config: {
-        backend: {
-          name: 'git-gateway',
-          branch: 'master',
-        },
-        adminPath: 'admin',
-        collections: [
-          {
-            name: 'posts',
-            label: 'Blog Posts',
-            folder: 'src/pages/posts',
-            create: true,
-            delete: true,
-            fields: [
-              { name: 'title', widget: 'string', label: 'Post Title' },
-              { name: 'body', widget: 'markdown', label: 'Post Body' },
-            ],
-          },
-        ],
+  integrations: [tailwind(), NetlifyCMS({
+    config: {
+      backend: {
+        name: 'git-gateway',
+        branch: 'master'
       },
-    }),
-  ],
-  output: 'server',
+      adminPath: 'admin',
+      collections: [{
+        name: 'posts',
+        label: 'Blog Posts',
+        folder: 'src/pages/posts',
+        create: true,
+        delete: true,
+        fields: [{
+          name: 'title',
+          widget: 'string',
+          label: 'Post Title'
+        }, {
+          name: 'body',
+          widget: 'markdown',
+          label: 'Post Body'
+        }]
+      }]
+    }
+  }), compress(), react()],
+  output: 'server'
 });
