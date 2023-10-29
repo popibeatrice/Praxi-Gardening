4ba004fab791b5eceaefddffd9c7ffffca0022a3
popibeatrice <popicabeatrice31@gmail.com>
2023-10-29 14:20:27 +0200

    .

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from 'astro/config';
+import NetlifyCMS from 'astro-netlify-cms';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'master',
+        },
+        adminPath: 'admin',
+        collections: [
+          {
+            name: 'posts',
+            label: 'Blog Posts',
+            folder: 'src/pages/posts',
+            create: true,
+            delete: true,
+            fields: [
+              { name: 'title', widget: 'string', label: 'Post Title' },
+              { name: 'body', widget: 'markdown', label: 'Post Body' },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
+  output: 'server',
+});
