dca6049de9df069a724871afcc94c5fe22cd00eb
popibeatrice <popicabeatrice31@gmail.com>
2023-10-31 20:32:03 +0200

    landing design and installation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,15 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      "lg-xl": "1050px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       fontFamily: {
         montserrat: ["Montserrat Variable", "sans-serif"],
