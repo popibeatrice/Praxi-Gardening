93f9827853ba5cd86fb7e47420bd42bb2b0b8e06
DavidPav3l <david_marian@outlook.com>
2023-11-06 22:58:14 +0200

    uau

--- src/components/ServicesAcc.jsx
+++ src/components/ServicesAcc.jsx
@@ -5,8 +5,6 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
-import fountain from "../assets/fountain.png";
-
 export default function ServicesAcc({ services }) {
   return (
     <Accordion collapsible>
@@ -15,8 +13,12 @@ export default function ServicesAcc({ services }) {
           <AccordionItem key={service.title} value={service.title}>
             <AccordionTrigger className="text-left hover:no-underline">
               <div className="flex items-center gap-5">
-                <img src={service.icon.src} alt="fountain" className="w-10" />
-                <h3 className="text-2xl">{service.title}</h3>
+                <img
+                  src={service.icon.src}
+                  alt="service icon"
+                  className="w-10 lg:w-12"
+                />
+                <h3 className="text-2xl lg:text-3xl">{service.title}</h3>
               </div>
             </AccordionTrigger>
             <AccordionContent>{service.parag}</AccordionContent>
