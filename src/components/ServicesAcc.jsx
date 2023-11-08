f830963cbd5fd0ee44440fc4731cf28d45d65f45
DavidPav3l <david_marian@outlook.com>
2023-11-08 21:10:52 +0200

    uau

--- src/components/ServicesAcc.jsx
+++ src/components/ServicesAcc.jsx
@@ -9,20 +9,19 @@ export default function ServicesAcc({ services }) {
   return (
     <Accordion type="single" collapsible>
       {services.map((service, index) => {
-        console.log(service.serviceIcon);
         return (
           <AccordionItem key={index + 1} value={index + 1}>
             <AccordionTrigger className="text-left hover:no-underline">
               <div className="flex items-center gap-5">
-                <img
+                {/* <img
                   src={service.serviceIcon}
                   alt="service icon"
                   className="w-10 lg:w-12"
-                />
-                <h3 className="text-2xl lg:text-3xl">{service.serviceName}</h3>
+                /> */}
+                <h3 className="text-2xl lg:text-3xl">placeholder</h3>
               </div>
             </AccordionTrigger>
-            <AccordionContent>{service.serviceDescription}</AccordionContent>
+            <AccordionContent>placeholder</AccordionContent>
           </AccordionItem>
         );
       })}
